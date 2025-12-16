9e8412621a3da3f2fda698a063ea6ef2ffc22b2d
gabrielee5 <fabiettigabriele@gmail.com>
2025-12-16 16:50:34 +0100

    moidfied default port

--- contrarian/core/web_dashboard.py
+++ contrarian/core/web_dashboard.py
@@ -29,7 +29,7 @@ class WebDashboard:
     def __init__(
         self,
         host: str = '127.0.0.1',
-        port: int = 5001,
+        port: int = 5002,
         priority_coins: List[str] = None
     ):
         """
